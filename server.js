e847e605dc6eaf1c75e325a93efc0cc1dea02e39
Max McCord <maxdmccord@gmail.com>
2019-12-17 17:06:30 -0600

    environment variable for port needed updating

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3033;
+const PORT = process.env.PORT || 3033;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
